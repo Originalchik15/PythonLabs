0fc9b7e7a93ae6744338857ce62dadb0f055e2fa
Originalchik15 <136095159+Originalchik15@users.noreply.github.com>
2024-11-22 13:47:19 +0400

    Update Lab-4.py

--- Lab-4/Lab-4.py
+++ Lab-4/Lab-4.py
@@ -2,6 +2,7 @@
 Заменять четные цифры, стоящие на нечетных местах словами 
 (цифры от 0 до 9 на слова «ноль», «один», …, «девять»)."""
 import re
+
 digit_to_word = {
     '0': 'ноль',
     '2': 'два',
@@ -12,23 +13,31 @@ digit_to_word = {
 def check_num(n):
     transformed = False  
     result = '' 
+    is_negative = n[0] == '-' 
+    if is_negative:
+        n = n[1:]  
     for i, char in enumerate(n):
         if char.isdigit(): 
-            if int(char) % 2 == 0 and (i + 1) % 2 != 0:  
+            if int(char) % 2 == 0 and (i + 1) % 2 != 0: 
                 result += digit_to_word[char]
                 transformed = True
-            else: result += char    
-        else: result += char        
+            else:
+                result += char    
+        else:
+            result += char        
+    if is_negative:
+        result = '-' + result
     return result, transformed
 def split_alot(num_input):
-    numbers = re.findall(r'\b\d+\b', num_input)  
+    numbers = re.findall(r'-?\d*[02468]\b', num_input)
     output = []
     for num in numbers:
         transformed_number, is_transformed = check_num(num)
-        if is_transformed:  output.append(transformed_number)
-    print(' '.join(output))  
+        if is_transformed:
+            output.append(transformed_number)
+    print(' '.join(output))
 while True:
-    with open('Lab-4/input.txt', 'r') as f:
+    with open('Lab-4/input.txt', 'r', encoding='utf-8') as f:
         num_input = f.read()
     split_alot(num_input)
     break
